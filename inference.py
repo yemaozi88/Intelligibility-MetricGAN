c1f02d317bf8b0e5824af764c485a1ff831c60ea
Aki <aki.kunikoshi@readspeaker.com>
2021-03-02 00:05:00 +0100

    inference on demo data.

--- inference.py
+++ inference.py
@@ -11,41 +11,53 @@ import os
 import librosa
 import numpy as np
 
-chkpt_path = './trained_model/chkpt_26.pt' # location of trained model
-# locations of enhanced output files 
-mod_path = './inference/English/Modify'
-raw_path = './inference/English/Clean'
-noi_path = './inference/English/Noise'
+import default_settings as default
 
-os.makedirs(mod_path, exist_ok=True)
-os.makedirs(raw_path, exist_ok=True)
-os.makedirs(noi_path, exist_ok=True)
 
-batch_size = 1
-fs = 44100
+##### MODIFY FROM HERE #####
+#chkpt_path = './trained_model/chkpt_26.pt' # location of trained model
+chkpt_path = r'/home/akikun/projects/nele/experiments/imgan_siib/checkpoint/chkpt_9.pt'
+root_dir = r'/home/common/db/audio_corpora/nele/jr_demo'
+Test_Noise_path = os.path.join(root_dir, 'noise')
+Test_Clean_path = os.path.join(root_dir, 'clean')
+enhanced_dir = os.path.join(root_dir, 'enhanced')
+##### MODIFY TILL HERE #####
 
+
+#batch_size = default.batch_size
+#fs = 44100
+fs = default.sampling_frequency
 
 print('Reading path of processing data...')
-Test_Noise_path = '/home/takkan/repos/Intelligibility-MetricGAN/database/Test/Noise/'
-Test_Clean_path = '/home/takkan/repos/Intelligibility-MetricGAN/database/Test/Clean/'
 Generator_Test_paths = get_filepaths(Test_Clean_path)
+#print(Generator_Test_paths)
 
+# locations of enhanced output files 
+if not os.path.exists(enhanced_dir):
+    os.makedirs(enhanced_dir)
+# mod_path = './inference/English/Modify'
+# raw_path = './inference/English/Clean'
+# noi_path = './inference/English/Noise'
+
+# os.makedirs(mod_path, exist_ok=True)
+# os.makedirs(raw_path, exist_ok=True)
+# os.makedirs(noi_path, exist_ok=True)
 
 print('Load Model...')
 G = Generator().cuda()
 G.load_state_dict(torch.load(chkpt_path)['enhance-model'])
 
-
-
 print("Processing...")
 G.eval()
 with torch.no_grad():
     for path in tqdm(Generator_Test_paths):
-        S = path.split('/')
-        wave_name = S[-1]
+        #S = path.split('/')
+        #wave_name = S[-1]
+        wave_name = os.path.basename(path)
+        print(wave_name)
 
         clean_wav,_ = librosa.load(path, sr=fs)
-        noise_wav,_ = librosa.load(Test_Noise_path+wave_name, sr=fs)
+        noise_wav,_ = librosa.load(os.path.join(Test_Noise_path, wave_name), sr=fs)
 
         noise_mag,noise_phase = Sp_and_phase(noise_wav, Normalization=True)
         clean_mag,clean_phase = Sp_and_phase(clean_wav, Normalization=True)
@@ -59,8 +71,8 @@ with torch.no_grad():
         beta_2 = torch.sum(clean_power) / torch.sum(torch.pow(mask,2)*clean_power)
         beta_p = beta_2 ** (0.30/2)
         mask = torch.pow(mask, 0.30) * beta_p
-        # Do not change high frequency components in inference stage, since they do not affect intelligibility actually
-        mask[0,:,380:] = 1.0
+        ## Do not change high frequency components in inference stage, since they do not affect intelligibility actually
+        #mask[0,:,380:] = 1.0
 
         enh_mag = clean_in * mask
         enh_mag = (enh_mag**(1/0.30)).detach().cpu().squeeze(0).numpy()
@@ -69,6 +81,6 @@ with torch.no_grad():
         enh_wav = np.hstack((enh_wav,np.zeros(len(clean_wav)-len(enh_wav),dtype=np.float32)))
         enh_wav = enh_wav / np.std(enh_wav) * np.std(clean_wav)
         
-        librosa.output.write_wav(os.path.join(mod_path, wave_name), enh_wav, fs)
-        librosa.output.write_wav(os.path.join(raw_path, wave_name), clean_wav, fs)
-        librosa.output.write_wav(os.path.join(noi_path, wave_name), noise_wav, fs)
+        librosa.output.write_wav(os.path.join(enhanced_dir, wave_name), enh_wav, fs)
+#         librosa.output.write_wav(os.path.join(raw_path, wave_name), clean_wav, fs)
+#         librosa.output.write_wav(os.path.join(noi_path, wave_name), noise_wav, fs)
